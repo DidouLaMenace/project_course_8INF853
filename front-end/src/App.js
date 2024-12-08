bd5d774b5f9b0ff75b3032b5d438903b2758ba2f
Dylan Fournier <dfournier1305@gmail.com>
2024-12-07 23:44:37 -0500

    Front-end

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import AuthPage from './components/AuthPage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './Pages/Home';
+import Login from './Pages/Login';
+import Register from './Pages/Register';
+import Catalogue from './Pages/Catalogue';
+import Reservation from './Pages/Reservation';
+import Dashboard from './Pages/User/Dashboard';
 
-const App = () => {
-    return (
-        <div>
-            <AuthPage />
-        </div>
-    );
-};
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/catalogue' element={<Catalogue />} />
+        <Route path='/reservation' element={<Reservation />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 export default App;
